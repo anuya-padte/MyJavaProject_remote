eefa96167dff132b5df01816de2f72984a245b14
AnuyaPadte <aspadte17@gmail.com>
2020-11-19 17:50:29 +0530

    Second commit

--- MyJavaProject/src/com/lti/demo/UserApp.java
+++ MyJavaProject/src/com/lti/demo/UserApp.java
@@ -4,5 +4,7 @@ public class UserApp {
 	public static void main(String[] args) {
 		Person p = new Person("Jay", 35, 90.00);
 		System.out.println(p);
+		Person p1 = new Person("Vijay", 48, 99.0);
+		System.out.println(p1);
 	}
 }
